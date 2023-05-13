1860c5ed3f0b87b1032e5957465763450b01e592
andreabellome <67928580+andreabellome@users.noreply.github.com>
2023-05-13 10:36:49 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+import { store } from './store';
+import Component from './components/Component.vue';
+
+export default {
+  name: "App",
+  components: {
+    Component
+  },
+  data() {
+    return {
+      store
+    }
+  }
+}
+
+</script>
+
+<template>
+  <Component />
+</template>
+
+<style lang="scss">
+@use "./style/main.scss" as *;
+</style>
---
+++ src/components/Component.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="debug text-center">
+        hello world this is a component
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Component',
+    setup() {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
