97abe16b65d82a82b74a8f6d03ab2c4d01e898e1
Andrea Bellome <andrea.bellome@gmail.com>
2023-05-13 12:28:31 +0200

    added imgs and svgs to assets

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -2,10 +2,13 @@
     <div class="debug padd-10">
         <div class="debug padd-10 width-75 margin-auto flex flex-dir-row just-cont-between">
 
+            <!-- start: logo -->
             <div class="debug">
                 chicco
             </div>
+            <!-- end: logo -->
 
+            <!-- start: dyanmic header with info from store -->
             <div class="debug">
                 <ul>
                     <li v-for="(elem, index) in datiHeader">
@@ -13,11 +16,13 @@
                     </li>
                 </ul>
             </div>
+            <!-- end: dyanmic header with info from store -->
 
-
+            <!-- start: icons -->
             <div class="debug">
                 chicco
             </div>
+            <!-- end: icons -->
 
 
         </div>
